755f874c60fd43b6c77730084c93e554254fa534
Esperanza Duenias <esperanza.duenas@unplug.studio>
2019-11-14 09:45:26 -0600

    Se modifico el sidebar

--- dashboard/src/app/sidebar/sidebar.component.ts
+++ dashboard/src/app/sidebar/sidebar.component.ts
@@ -9,14 +9,9 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
-    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
-    { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-chart-bar-32', class: '' },
+    { path: '/icons',         title: 'Temperatura',       icon:'nc-bulb-63',      class: '' },
+    { path: '/maps',          title: 'Humedad',           icon:'nc-world-2',      class: '' }
 ];
 
 @Component({
