b0eab473977402e47472c60545e628845a181bc1
Esperanza Duenas <da13002@ues.edu.sv>
2019-11-19 22:22:53 -0600

    Metodos para crud

---
+++ dashboard/src/app/services/crud.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(
+    private firestore: AngularFirestore
+  ) {}
+
+  public createCat(data: any) {
+    return this.firestore.collection('data').add(data);
+  }
+
+  public getCat(documentId: string) {
+    return this.firestore.collection('data').doc(documentId).snapshotChanges();
+  }
+
+  public getCats() {
+    return this.firestore.collection('data').snapshotChanges();
+  }
+
+  public updateCat(documentId: string, data: any) {
+    return this.firestore.collection('data').doc(documentId).set(data);
+  }
+
+  public deleteCat(documentId: string) {
+    return this.firestore.collection('data').doc(documentId).delete();
+  }
+}
